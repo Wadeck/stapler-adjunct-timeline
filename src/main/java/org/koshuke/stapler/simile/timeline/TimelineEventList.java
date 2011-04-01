6ee2f923807353a9ec49a57ecd358d77339e5b1a
Matthias Kraft <Matthias.Kraft@softwareag.com>
2011-04-01 14:31:39 +0200

    JENKINS-6439: Fixed timestamp format.
    The Timeline widget requires specially formated timestamps. Especially
    the timezone has to be like "GMT<+offset>".
    
    Signed-off-by: Matthias Kraft <Matthias.Kraft@softwareag.com>

--- src/main/java/org/koshuke/stapler/simile/timeline/TimelineEventList.java
+++ src/main/java/org/koshuke/stapler/simile/timeline/TimelineEventList.java
@@ -10,13 +10,16 @@ import org.kohsuke.stapler.StaplerResponse;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * List of {@link Event} that the timeline component will display.
  */
 public class TimelineEventList extends ArrayList<Event> implements HttpResponse {
+    private static SimpleDateFormat SDF = new SimpleDateFormat("MMM dd yyyy HH:mm:ss 'GMT'Z", Locale.ENGLISH);
     /**
      * Renders HTTP response.
      */
@@ -25,7 +28,7 @@ public class TimelineEventList extends ArrayList<Event> implements HttpResponse 
         JsonConfig config = new JsonConfig();
         config.registerJsonValueProcessor(Date.class,new JsonValueProcessor() {
             public Object processArrayValue(Object value, JsonConfig jsonConfig) {
-                return value==null ? null : value.toString();
+                return value==null ? null : SDF.format(value);
             }
 
             public Object processObjectValue(String key, Object value, JsonConfig jsonConfig) {
